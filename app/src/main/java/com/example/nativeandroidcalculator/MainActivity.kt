dd15fd2ecf3b82940ee1242bcfb1a13caecd03c9
dozy <dozy@Dozys-MacBook-Pro.local>
2024-08-02 12:18:03 +0100

    added some functionalities to the calculator.

--- app/src/main/java/com/example/nativeandroidcalculator/MainActivity.kt
+++ app/src/main/java/com/example/nativeandroidcalculator/MainActivity.kt
@@ -16,9 +16,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,6 +30,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import net.objecthunter.exp4j.ExpressionBuilder
 import com.example.nativeandroidcalculator.ui.theme.NativeAndroidCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +39,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NativeAndroidCalculatorTheme {
-                Surface (
+                Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
                     NativeAndroidCalculator()
@@ -48,82 +52,158 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun NativeAndroidCalculator() {
     val DarkGreen = Color(0xFF006400)
-    Column (
+    val DividerColor = Color(0xFFD3D3D3)
+    val inputState = remember { mutableStateOf("") }
+    val isPlusState = remember { mutableStateOf(true) }
+
+    Column(
         modifier = Modifier.padding(30.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom
     ) {
-        Row (modifier = Modifier
-            .padding(bottom = 35.dp)
-            .fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(
+                text = inputState.value,
+                fontSize = 24.sp,
+                color = DividerColor,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .padding(bottom = 30.dp, start = 20.dp, end = 20.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
             IconButton(iconResId = R.drawable.clock_icon)
             IconButton(iconResId = R.drawable.metric_icon)
             IconButton(iconResId = R.drawable.orientation_icon)
             Spacer(modifier = Modifier.weight(1f))
             IconButton(iconResId = R.drawable.backspace_icon)
         }
-        Row (modifier = Modifier
-            .padding(bottom = 16.dp)
-            .fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-            CalculatorButton(text = stringResource(R.string.C), textColor = Color.Red)
-            CalculatorButton(text = stringResource(R.string.bracket_sign), textColor = DarkGreen)
-            CalculatorButton(text = stringResource(R.string.percentage_sign), textColor = DarkGreen)
-            CalculatorButton(text = stringResource(R.string.division), textColor = DarkGreen)
+        Divider(color = DividerColor, thickness = 1.dp, modifier = Modifier.padding(bottom = 16.dp))
+        Row(
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            CalculatorButton(text = stringResource(R.string.C), textColor = Color.Red) {
+                inputState.value = ""
+            }
+            CalculatorButton(text = stringResource(R.string.bracket_sign), textColor = DarkGreen) {
+                inputState.value += "("
+            }
+            CalculatorButton(text = stringResource(R.string.percentage_sign), textColor = DarkGreen) {
+                inputState.value += "%"
+            }
+            CalculatorButton(text = stringResource(R.string.division), textColor = DarkGreen) {
+                inputState.value += "/"
+            }
         }
-        Row (modifier = Modifier
-            .padding(bottom = 16.dp)
-            .fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-            CalculatorButton(text = stringResource(R.string.no_7))
-            CalculatorButton(text = stringResource(R.string.no_8))
-            CalculatorButton(text = stringResource(R.string.no_9))
-            CalculatorButton(text = stringResource(R.string.no_x), textColor = DarkGreen)
+        Row(
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            CalculatorButton(text = stringResource(R.string.no_7)) {
+                inputState.value += "7"
+            }
+            CalculatorButton(text = stringResource(R.string.no_8)) {
+                inputState.value += "8"
+            }
+            CalculatorButton(text = stringResource(R.string.no_9)) {
+                inputState.value += "9"
+            }
+            CalculatorButton(text = stringResource(R.string.no_x), textColor = DarkGreen) {
+                inputState.value += "*"
+            }
         }
-        Row (modifier = Modifier
-            .padding(bottom = 16.dp)
-            .fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-            CalculatorButton(text = stringResource(R.string.no_4))
-            CalculatorButton(text = stringResource(R.string.no_5))
-            CalculatorButton(text = stringResource(R.string.no_6))
-            CalculatorButton(text = stringResource(R.string.substraction), textColor = DarkGreen)
+        Row(
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            CalculatorButton(text = stringResource(R.string.no_4)) {
+                inputState.value += "4"
+            }
+            CalculatorButton(text = stringResource(R.string.no_5)) {
+                inputState.value += "5"
+            }
+            CalculatorButton(text = stringResource(R.string.no_6)) {
+                inputState.value += "6"
+            }
+            CalculatorButton(text = stringResource(R.string.substraction), textColor = DarkGreen) {
+                inputState.value += "-"
+            }
         }
-        Row (modifier = Modifier
-            .padding(bottom = 16.dp)
-            .fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-            CalculatorButton(text = stringResource(R.string.no_1))
-            CalculatorButton(text = stringResource(R.string.no_2))
-            CalculatorButton(text = stringResource(R.string.no_3))
-            CalculatorButton(text = stringResource(R.string.addition), textColor = DarkGreen)
+        Row(
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            CalculatorButton(text = stringResource(R.string.no_1)) {
+                inputState.value += "1"
+            }
+            CalculatorButton(text = stringResource(R.string.no_2)) {
+                inputState.value += "2"
+            }
+            CalculatorButton(text = stringResource(R.string.no_3)) {
+                inputState.value += "3"
+            }
+            CalculatorButton(text = stringResource(R.string.addition), textColor = DarkGreen) {
+                inputState.value += "+"
+            }
         }
-        Row (modifier = Modifier
-            .padding(bottom = 16.dp)
-            .fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-            CalculatorButton(text = stringResource(R.string.addition_substraction))
-            CalculatorButton(text = stringResource(R.string.no_0))
-            CalculatorButton(text = stringResource(R.string.fullstop))
-            CalculatorButton(text = stringResource(R.string.equal_to), textColor = Color.White, buttonColor = DarkGreen)
+        Row(
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            CalculatorButton(text = stringResource(R.string.addition_substraction)) {
+                if (isPlusState.value) {
+                    inputState.value = "+"
+                } else {
+                    inputState.value = "-"
+                }
+                isPlusState.value = !isPlusState.value
+            }
+            CalculatorButton(text = stringResource(R.string.no_0)) {
+                inputState.value += "0"
+            }
+            CalculatorButton(text = stringResource(R.string.fullstop)) {
+                inputState.value += "."
+            }
+            CalculatorButton(text = stringResource(R.string.equal_to), textColor = Color.White, buttonColor = DarkGreen) {
+                inputState.value = evaluateExpression(inputState.value)
+            }
         }
     }
 }
 
 @Composable
-fun CalculatorButton (
-                      text: String,
-                      textColor: Color = Color.Black,
-                      buttonColor: Color = Color(0xFFF0F0F0)
-                    ) {
+fun CalculatorButton(
+    text: String,
+    textColor: Color = Color.Black,
+    buttonColor: Color = Color(0xFFF0F0F0),
+    onClick: () -> Unit
+) {
     Button(
-        onClick = { /*TODO*/ },
+        onClick = onClick,
         shape = CircleShape,
         colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
         modifier = Modifier.size(75.dp)
     ) {
-        Text(text = text,
+        Text(
+            text = text,
             fontSize = 24.sp,
             color = textColor
         )
@@ -131,11 +211,23 @@ fun CalculatorButton (
 }
 
 @Composable
-fun IconButton (
+fun IconButton(
     iconResId: Int = R.drawable.clock_icon,
 ) {
-    Image(painter = painterResource(id = iconResId),
-        contentDescription = null )
+    Image(painter = painterResource(id = iconResId), contentDescription = null)
+}
+
+fun evaluateExpression(expression: String): String {
+    return try {
+        val result = ExpressionBuilder(expression).build().evaluate()
+        if (result == result.toLong().toDouble()) {
+            result.toLong().toString()
+        } else {
+            result.toString()
+        }
+    } catch (e: Exception) {
+        "Error"
+    }
 }
 
 @Preview(showBackground = true)
